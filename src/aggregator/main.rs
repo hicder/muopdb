328d3eb9b349d32703468012a474d66d184685cb
Hieu Pham <dpham810@gmail.com>
2024-10-08 15:13:20 -0700

    Fix comment

--- src/aggregator/main.rs
+++ src/aggregator/main.rs
@@ -9,7 +9,6 @@ use tonic::transport::Server;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
     #[arg(short, long, default_value_t = 9001)]
     port: u32,
 }
