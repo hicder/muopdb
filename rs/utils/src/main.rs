d7e291b5d952655c7702fa55daa011c0ef5ffb7f
Hieu Pham <dpham810@gmail.com>
2024-11-09 06:48:53 -0800

    Remove uncessary hdf5 file

--- rs/utils/src/main.rs
+++
@@ -1,9 +0,0 @@
-use utils::hdf5_reader::read_hdf5_sift_128;
-
-fn main() {
-    let path = "/mnt/dataset/50M_embeddings_128.hdf5";
-    match read_hdf5_sift_128(path) {
-        Ok(_) => println!("HDF5 file read successfully!"),
-        Err(e) => println!("Error reading HDF5 file: {}", e),
-    }
-}
