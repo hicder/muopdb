5a0a2cd8b04a8640f0cd387e440e552c5667baf3
Vy Hong <shiweistg@gmail.com>
2024-11-09 22:06:36 -0800

    Calculate the centroid

--- rs/quantization/src/rabitq_builder.rs
+++ rs/quantization/src/rabitq_builder.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use bit_vec::BitVec;
-use ndarray::Array2;
+use ndarray::{Array1, Array2, Axis};
 use ndarray_rand::RandomExt;
 use ndarray_rand::rand_distr::StandardNormal;
 use ndarray_linalg::qr::QR;
@@ -9,7 +9,7 @@ use std::error::Error;
 
 pub struct RabitQBuilder {
     dimension: usize,
-    dataset: Vec<Vec<f32>>,
+    dataset: Vec<f32>,
 }
 
 impl RabitQBuilder {
@@ -23,15 +23,19 @@ impl RabitQBuilder {
     }
 
     pub fn add(&mut self, data: Vec<f32>) {
-        self.dataset.push(data);
-
+        self.dataset.append(&mut data.clone());
     }
 
     pub fn build(&mut self, _base_directory: String) -> Result<RabitQ, Box<dyn Error>> {
         let q = self.create_orthogonal_matrix(self.dimension)?;
 
+        let shape = (self.dataset.len() / self.dimension, self.dimension);
+        let dataset = Array2::from_shape_vec(shape, self.dataset.clone())?;
+        let centroid: Array1<f32> = dataset.mean_axis(Axis(0)).ok_or("Failed to calculate mean")?;
+
         Ok(RabitQ {
             p_inv: q.inv()?,
+            centroid,
             quantization_codes: Vec::new(),
             or_c: Vec::new(),
             o_o: Vec::new(),
@@ -47,6 +51,7 @@ impl RabitQBuilder {
 
 pub struct RabitQ {
     p_inv: Array2<f32>,
+    centroid: Array1<f32>,
     quantization_codes: Vec<BitVec>,
     or_c: Vec<f32>,
     o_o: Vec<Vec<f32>>,
