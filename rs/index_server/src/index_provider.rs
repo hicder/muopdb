0cfec04576382a928f141ea02b497857807d7053
BuildKite <buildkite@buildkite.com>
2024-12-20 21:39:52 +0100

    Write and read quantizer config to avoid hardcoding in index_provider

--- rs/index_server/src/index_provider.rs
+++ rs/index_server/src/index_provider.rs
@@ -2,7 +2,7 @@ use index::hnsw::reader::HnswReader;
 use index::index::BoxedIndex;
 use index::ivf::reader::IvfReader;
 use index::spann::reader::SpannReader;
-use index_writer::config::BaseConfig;
+use index_writer::config::{BaseConfig, QuantizerConfig, QuantizerType};
 use quantization::noq::noq::NoQuantizer;
 use quantization::pq::pq::ProductQuantizer;
 
@@ -17,31 +17,52 @@ impl IndexProvider {
 
     pub fn read_index(&self, name: &str) -> Option<BoxedIndex> {
         let index_path = format!("{}/{}", self.data_directory, name);
-        let index_writer_config_path = format!("{}/base_config.yaml", index_path);
+        let base_config_path = format!("{}/base_config.yaml", index_path);
+        let quantizer_config_path = format!("{}/quantizer_config.yaml", index_path);
 
         let base_config: BaseConfig =
-            serde_yaml::from_reader(std::fs::File::open(index_writer_config_path).unwrap())
-                .unwrap();
+            serde_yaml::from_reader(std::fs::File::open(base_config_path).unwrap()).unwrap();
+
+        let quantizer_config: QuantizerConfig =
+            serde_yaml::from_reader(std::fs::File::open(quantizer_config_path).unwrap()).unwrap();
 
         match base_config.index_type {
             index_writer::config::IndexType::Hnsw => {
                 let reader = HnswReader::new(index_path);
-                match reader.read::<ProductQuantizer>() {
-                    Ok(index) => Some(Box::new(index)),
-                    Err(e) => {
-                        println!("Failed to read index: {}", e);
-                        None
-                    }
+                match quantizer_config.quantizer_type {
+                    QuantizerType::ProductQuantizer => match reader.read::<ProductQuantizer>() {
+                        Ok(index) => Some(Box::new(index)),
+                        Err(e) => {
+                            println!("Failed to read ProductQuantizer index: {}", e);
+                            None
+                        }
+                    },
+                    QuantizerType::NoQuantizer => match reader.read::<NoQuantizer>() {
+                        Ok(index) => Some(Box::new(index)),
+                        Err(e) => {
+                            println!("Failed to read NoQuantizer index: {}", e);
+                            None
+                        }
+                    },
                 }
             }
             index_writer::config::IndexType::Ivf => {
                 let reader = IvfReader::new(index_path);
-                match reader.read::<NoQuantizer>() {
-                    Ok(index) => Some(Box::new(index)),
-                    Err(e) => {
-                        println!("Failed to read index: {}", e);
-                        None
-                    }
+                match quantizer_config.quantizer_type {
+                    QuantizerType::ProductQuantizer => match reader.read::<ProductQuantizer>() {
+                        Ok(index) => Some(Box::new(index)),
+                        Err(e) => {
+                            println!("Failed to read ProductQuantizer index: {}", e);
+                            None
+                        }
+                    },
+                    QuantizerType::NoQuantizer => match reader.read::<NoQuantizer>() {
+                        Ok(index) => Some(Box::new(index)),
+                        Err(e) => {
+                            println!("Failed to read NoQuantizer index: {}", e);
+                            None
+                        }
+                    },
                 }
             }
             index_writer::config::IndexType::Spann => {
