bf2710b1aed498bc2e17543c8059b18e2205d4ad
BuildKite <buildkite@buildkite.com>
2025-01-01 23:38:48 +0100

    Encoder's len should return byte size instead of num elem

--- rs/compression/src/compression.rs
+++ rs/compression/src/compression.rs
@@ -5,16 +5,17 @@ use anyhow::Result;
 
 pub trait IntSeqEncoder {
     /// Creates an encoder
-    fn new_encoder(universe: Option<usize>, size: usize) -> Self
+    fn new_encoder(universe: Option<usize>, num_elem: usize) -> Self
     where
         Self: Sized;
 
     /// Compresses a sorted slice of integers
     fn encode(&mut self, values: &[u64]) -> Result<()>;
 
-    /// Returns the number of elements in the sequence
+    /// Returns the size of the encoded data (that would be written to disk)
     fn len(&self) -> usize;
 
-    /// Writes to disk and return number of bytes written.
+    /// Writes to disk and returns number of bytes written (which can be just len(),
+    /// or more if extra info is also required for decoding)
     fn write(&self, writer: &mut BufWriter<&mut File>) -> Result<usize>;
 }
