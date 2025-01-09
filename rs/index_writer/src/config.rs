2bdcceb33166ab58b87d742fde604355f0e1c218
BuildKite <buildkite@buildkite.com>
2025-01-09 16:52:35 +0100

    Move enum type to a common place

--- rs/index_writer/src/config.rs
+++ rs/index_writer/src/config.rs
@@ -1,35 +1,5 @@
+use config::enums::{DistanceType, IndexType, IntSeqEncodingType, QuantizerType};
 use serde::{Deserialize, Serialize};
-
-// TODO(hicder): support more quantizers
-#[derive(Debug, Clone, Default, Serialize, Deserialize, PartialEq)]
-pub enum QuantizerType {
-    ProductQuantizer,
-    #[default]
-    NoQuantizer,
-}
-
-#[derive(Debug, Clone, Default, Serialize, Deserialize, PartialEq)]
-pub enum DistanceType {
-    DotProduct,
-    #[default]
-    L2,
-}
-
-// TODO(tyb): support more encoding
-#[derive(Debug, Clone, Default, Serialize, Deserialize, PartialEq)]
-pub enum IntSeqEncodingType {
-    EliasFano,
-    #[default]
-    PlainEncoding,
-}
-
-#[derive(Debug, Clone, Default, Serialize, Deserialize, PartialEq)]
-pub enum IndexType {
-    Hnsw,
-    Ivf,
-    #[default]
-    Spann,
-}
 
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct BaseConfig {
