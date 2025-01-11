d694a74fa5b1bf241bdf088e47d96b90002d446b
BuildKite <buildkite@buildkite.com>
2025-01-11 22:39:35 +0100

    Document part of collection/segment

--- rs/index/src/segment/mod.rs
+++ rs/index/src/segment/mod.rs
@@ -3,13 +3,15 @@ pub mod mutable_segment;
 
 use anyhow::Result;
 
+/// A segment is a partial index: users can insert some documents, then flush
+/// the containing collection, to effectively create a segment.
 pub trait Segment {
-    /// Insert a document into the segment.
+    /// Inserts a document into the segment.
     /// Returns an error if the insertion process fails for any reason.
     /// NOTE: Some type of segment may not support insertion.
     fn insert(&mut self, doc_id: u64, data: &[f32]) -> Result<()>;
 
-    /// Remove a document from the segment.
+    /// Removes a document from the segment.
     /// Returns true if the document was removed, false if the document was not found.
     /// Returns an error if the removal process fails for any reason.
     fn remove(&mut self, doc_id: u64) -> Result<bool>;
