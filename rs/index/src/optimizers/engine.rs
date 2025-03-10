90beff664d97044074b24d55f1c9226819bbdc34
Khoa Le <khoatle165@gmail.com>
2025-03-10 02:07:21 -0400

    Add vacuum optimizer

--- rs/index/src/optimizers/engine.rs
+++ rs/index/src/optimizers/engine.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 use anyhow::{Ok, Result};
 use quantization::quantization::Quantizer;
 
+use super::vacuum::VacuumOptimizer;
 use super::merge::MergeOptimizer;
 use super::noop::NoopOptimizer;
 use crate::collection::collection::Collection;
@@ -27,10 +28,19 @@ impl<Q: Quantizer + Clone + Send + Sync + 'static> OptimizerEngine<Q> {
         if segments.len() < 2 && optimizing_type == OptimizingType::Merge {
             return Ok("".to_string());
         }
+        if segments.len() > 1 && optimizing_type == OptimizingType::Vacuum {
+            return Ok("".to_string());
+        }
 
         let pending_segment = self.collection.init_optimizing(&segments)?;
         match optimizing_type {
-            OptimizingType::Vacuum => Ok("".to_string()),
+            OptimizingType::Vacuum => {
+                let vacuum_optimizer = VacuumOptimizer::<Q>::new();
+                let new_segment_name = self
+                    .collection
+                    .run_optimizer(&vacuum_optimizer, &pending_segment)?;
+                Ok(new_segment_name)
+            }
             OptimizingType::Merge => {
                 let merge_optimizer = MergeOptimizer::<Q>::new();
                 let new_segment_name = self
