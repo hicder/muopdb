67e6b29a0494a371ef24bf834a62df62fb409ef1
Hieu Pham <dpham810@gmail.com>
2025-03-14 09:14:13 -0700

    Avoid grabbing write lock unnecessary

--- rs/index/src/segment/mutable_segment.rs
+++ rs/index/src/segment/mutable_segment.rs
@@ -33,7 +33,7 @@ impl MutableSegment {
         self.created_at
     }
 
-    pub fn insert(&mut self, doc_id: u128, data: &[f32]) -> Result<()> {
+    pub fn insert(&self, doc_id: u128, data: &[f32]) -> Result<()> {
         if self.finalized {
             return Err(anyhow::anyhow!("Cannot insert into a finalized segment"));
         }
